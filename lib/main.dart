30a5f80aea45c2d4d7849086eb72cfcc2e69609b
Yasiru Dahanayaka <yasirudahanayaka19@gmail.com>
2022-06-05 18:28:05 +0530

    Flutter MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,51 @@
-import 'dart:html';
 
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 
 void main(){
-  runApp(MyAppTwo());
+  runApp(
+      DevicePreview(
+        enabled: !kReleaseMode,
+        builder: (context) => MyApp(), // Wrap your app
+      )
+  );
 }
+
+// class MyApp extends StatelessWidget{
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       home: Text("Hello Yasiru"),
+//     );
+//   }
+//
+// }
 
 class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Text("Hello Yasiru"),
+    return MaterialApp(
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      title: "Flutter Demo",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
     );
   }
 
 }
 
-class MyAppTwo extends StatelessWidget{
+class MyHomePage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Hello Flutter"),
-        ),
-        body: const Center(
-          child: Text("Copyright by Yasiru Dahanayaka",
-            style: TextStyle(fontSize: 20.0),
-          ),
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter"),
       ),
     );
   }
